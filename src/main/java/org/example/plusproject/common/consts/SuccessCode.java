c618cc9963fd37b16e48eb2b1d1583373b501750
zerone <bazz0324@naver.com>
2025-09-26 20:23:00 +0900

    feat: [product] 상품 등록 성공 코드 SuccessCode.PRODUCT_CREATED 추가

--- src/main/java/org/example/plusproject/common/consts/SuccessCode.java
+++ src/main/java/org/example/plusproject/common/consts/SuccessCode.java
@@ -8,7 +8,11 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum SuccessCode {
 
-    REQUEST_SUCCESS(HttpStatus.OK, "요청이 성공적으로 처리되었습니다.");
+    REQUEST_SUCCESS(HttpStatus.OK, "요청이 성공적으로 처리되었습니다."),
+
+    // product
+    PRODUCT_CREATED(HttpStatus.CREATED, "상품이 성공적으로 등록되었습니다.");
+
 
     private final HttpStatus httpStatus;
     private final String message;
---
+++ src/main/java/org/example/plusproject/domain/product/controller/AdminProductController.java
@@ -0,0 +1,37 @@
+package org.example.plusproject.domain.product.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.example.plusproject.common.consts.SuccessCode;
+import org.example.plusproject.common.dto.response.ApiResponse;
+import org.example.plusproject.domain.product.dto.request.ProductCreateRequest;
+import org.example.plusproject.domain.product.dto.response.ProductResponse;
+import org.example.plusproject.domain.product.service.command.ProductCommandService;
+import org.example.plusproject.domain.product.service.query.ProductQueryService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/admin/products")
+public class AdminProductController {
+
+    private final ProductCommandService productCommandService;
+
+    // 상품 등록
+    @PostMapping
+    public ResponseEntity<ApiResponse<ProductResponse>> createProduct(
+            @Valid @RequestBody ProductCreateRequest productCreateRequest) {
+
+        ProductResponse productResponse = productCommandService.createProduct(productCreateRequest);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                ApiResponse.of(SuccessCode.PRODUCT_CREATED, productResponse));
+    }
+}
