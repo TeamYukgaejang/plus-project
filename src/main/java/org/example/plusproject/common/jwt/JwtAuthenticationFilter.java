f6a51ce148f48e9b7be8490f71f7daf0540bc1c3
이지호 <leejiho0114@gmail.com>
2025-09-26 23:56:42 +0900

    fix : [common] @NonNull 경고 해결

--- src/main/java/org/example/plusproject/common/jwt/JwtAuthenticationFilter.java
+++ src/main/java/org/example/plusproject/common/jwt/JwtAuthenticationFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.plusproject.common.security.JwtAuthenticationToken;
@@ -29,7 +30,11 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final ObjectMapper objectMapper;
 
     @Override
-    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+    protected void doFilterInternal(
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain)
+            throws ServletException, IOException {
         String tokenValue = jwtUtil.getJwtFromHeader(request);
 
         if (tokenValue != null) {
