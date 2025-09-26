d67473c14dc7067512d7118e8cb63fe296fdc324
csy10001 <fuitshine@gmail.com>
2025-09-26 17:47:38 +0900

    fix:[search] ApiResponse에 따라 Search Controller fix

--- src/main/java/org/example/plusproject/common/consts/SuccessCode.java
+++ src/main/java/org/example/plusproject/common/consts/SuccessCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @RequiredArgsConstructor
 public enum SuccessCode {
 
-    REQUEST_SUCCESS(HttpStatus.OK, "요청이 성공적으로 처리되었습니다.");
+    REQUEST_SUCCESS(HttpStatus.OK, "요청이 성공적으로 처리되었습니다."),
+    SEARCH_SUCCESS(HttpStatus.OK, "검색이 성공적으로 처리되었습니다."),
+    SEARCH_TRENDING_SUCCESS(HttpStatus.OK, "인기 검색어 조회가 성공적으로 처리되었습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
