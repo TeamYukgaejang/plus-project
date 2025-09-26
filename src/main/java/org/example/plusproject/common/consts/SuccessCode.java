6d7b071411e04dcc11decf8516bd6939607e2665
이지호 <leejiho0114@gmail.com>
2025-09-26 22:21:54 +0900

    refactor : [common] API 응답 및 에러 코드 구조 리팩토링

--- src/main/java/org/example/plusproject/common/consts/SuccessCode.java
+++ src/main/java/org/example/plusproject/common/consts/SuccessCode.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 public enum SuccessCode {
 
     REQUEST_SUCCESS(HttpStatus.OK, "요청이 성공적으로 처리되었습니다."),
+    USER_CREATED(HttpStatus.CREATED, "회원가입이 성공적으로 완료되었습니다."),
     SEARCH_SUCCESS(HttpStatus.OK, "검색이 성공적으로 처리되었습니다."),
     SEARCH_TRENDING_SUCCESS(HttpStatus.OK, "인기 검색어 조회가 성공적으로 처리되었습니다.");
 
