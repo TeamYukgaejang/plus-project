0c47068df92e754c09b72fd12c3c2f3f5590faa6
zerone <bazz0324@naver.com>
2025-09-28 04:17:06 +0900

    refactor: [product] @AllArgsConstructor 추가 및 of() 메서드 개선

--- src/main/java/org/example/plusproject/domain/product/dto/request/ProductCreateRequest.java
+++ src/main/java/org/example/plusproject/domain/product/dto/request/ProductCreateRequest.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 public class ProductCreateRequest {
 
     @NotNull
@@ -27,11 +29,6 @@ public class ProductCreateRequest {
     private String content;
 
     public static ProductCreateRequest of(Long categoryId, String name, Integer price, String content) {
-        ProductCreateRequest productCreateRequest = new ProductCreateRequest();
-        productCreateRequest.categoryId = categoryId;
-        productCreateRequest.name = name;
-        productCreateRequest.price = price;
-        productCreateRequest.content = content;
-        return productCreateRequest;
+        return new ProductCreateRequest(categoryId, name, price, content);
     }
 }
