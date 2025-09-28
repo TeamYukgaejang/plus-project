839c5164a20ba124529bdac093abd7f187daec5b
zerone <bazz0324@naver.com>
2025-09-29 00:28:57 +0900

    feat: [product] ProductException, ProductErrorCode, ProductSuccessCode 생성

---
+++ src/main/java/org/example/plusproject/domain/product/exception/ProductException.java
@@ -0,0 +1,9 @@
+package org.example.plusproject.domain.product.exception;
+
+import org.example.plusproject.common.exception.GlobalException;
+
+public class ProductException extends GlobalException {
+    public ProductException(ProductErrorCode productErrorCode, ProductSuccessCode productSuccessCode) {
+        super(productErrorCode, productSuccessCode);
+    }
+}
