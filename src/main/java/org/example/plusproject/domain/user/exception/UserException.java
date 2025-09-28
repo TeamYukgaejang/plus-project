9cce3d60bae44a7117a2013e7d129f211faef03c
zerone <bazz0324@naver.com>
2025-09-29 00:27:22 +0900

    refactor: [user] UserErrorCode, UserSuccessCode enum 작성 및 수정

---
+++ src/main/java/org/example/plusproject/domain/user/exception/UserException.java
@@ -0,0 +1,9 @@
+package org.example.plusproject.domain.user.exception;
+
+import org.example.plusproject.common.exception.GlobalException;
+
+public class UserException extends GlobalException {
+    public UserException(UserErrorCode  userErrorCode, UserSuccessCode userSuccessCode) {
+        super(userErrorCode, userSuccessCode);
+    }
+}
