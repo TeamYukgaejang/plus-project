f7ec4bb51a88c6d9eddff4357f73d978a0b77e09
zerone <bazz0324@naver.com>
2025-09-29 09:31:17 +0900

    refactor: [product] ProductException 생성자에서 SuccessCode 제거 및 ErrorCode 전용 생성자 추가

--- src/main/java/org/example/plusproject/domain/product/exception/ProductException.java
+++ src/main/java/org/example/plusproject/domain/product/exception/ProductException.java
@@ -3,6 +3,10 @@ package org.example.plusproject.domain.product.exception;
 import org.example.plusproject.common.exception.GlobalException;
 
 public class ProductException extends GlobalException {
+    public ProductException(ProductErrorCode productErrorCode) {
+        super(productErrorCode);
+    }
+
     public ProductException(ProductErrorCode productErrorCode, ProductSuccessCode productSuccessCode) {
         super(productErrorCode, productSuccessCode);
     }
