0b397d830f1065f2638a404b8ca1c3ae2a09b13e
zerone <bazz0324@naver.com>
2025-09-29 20:55:39 +0900

    feat: [product] category와 review 연관관계 설정

--- src/main/java/org/example/plusproject/domain/product/entity/Product.java
+++ src/main/java/org/example/plusproject/domain/product/entity/Product.java
@@ -3,6 +3,11 @@ package org.example.plusproject.domain.product.entity;
 import jakarta.persistence.*;
 import lombok.*;
 import org.example.plusproject.common.entity.BaseRemovableEntity;
+import org.example.plusproject.domain.category.entity.Category;
+import org.example.plusproject.domain.review.entity.Review;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Entity
@@ -16,12 +21,9 @@ public class Product extends BaseRemovableEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private Long categoryId;
-    // 연관관계 설정 예정
-//    @ManyToOne(fetch = FetchType.LAZY)
-//    @JoinColumn(name = "category_id", nullable = false)
-//    private Category category;
-
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
 
     @Column(nullable = false, length = 100)
     private String name;
@@ -34,24 +36,27 @@ public class Product extends BaseRemovableEntity {
 
     private int reviewCount;
 
+    @OneToMany(mappedBy = "product", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Review> reviews = new ArrayList<>();
+
     @Builder
-    private Product(String name, int price, String content, int reviewCount, long categoryId) {
+    private Product(String name, int price, String content, int reviewCount, Category category) {
         this.name = name;
         this.content = content;
         this.price = price;
         this.reviewCount = reviewCount;
-        this.categoryId = categoryId;
+        this.category = category;
     }
 
     // reviewCount를 0으로 초기화
-    public static Product of(String name, int price, String content, long categoryId) {
-        return new Product(name, price, content, 0, categoryId);
+    public static Product of(String name, int price, String content, Category category) {
+        return new Product(name, price, content, 0, category);
     }
 
-    public void update(String name, int price, String content, Long categoryId) {
+    public void update(String name, int price, String content, Category category) {
         this.name = name;
         this.price = price;
         this.content = content;
-        this.categoryId = categoryId;
+        this.category = category;
     }
 }
