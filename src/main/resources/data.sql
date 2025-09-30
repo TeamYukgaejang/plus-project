20fca8e57adeca110967dc0c11e765645bfe038a
zerone <bazz0324@naver.com>
2025-09-30 20:38:15 +0900

    docs: [product] 인기 상품 조회 리뷰수 테스트용 파일 추가

---
+++ src/main/resources/data.sql
@@ -0,0 +1,6 @@
+# -- data.sql
+# UPDATE products SET review_count = 50 WHERE id = 2;
+# UPDATE products SET review_count = 120 WHERE id = 3;
+# UPDATE products SET review_count = 30 WHERE id = 4;
+# UPDATE products SET review_count = 200 WHERE id = 5;
+# UPDATE products SET review_count = 10 WHERE id = 6;
