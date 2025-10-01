a9515e28f90cfac444f4a9b03d370d41f31b9cda
zerone <bazz0324@naver.com>
2025-10-02 06:59:20 +0900

    feat: [product] ProductRepository에 조회순, 조회수 쿼리문 추가

--- src/main/java/org/example/plusproject/domain/product/repository/ProductRepository.java
+++ src/main/java/org/example/plusproject/domain/product/repository/ProductRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
@@ -15,8 +16,26 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
     // name 기준 LIKE 검색
     Page<Product> findByNameContaining(String keyword, Pageable pageable);
 
+    // 리뷰순
     @EntityGraph(attributePaths = {"category"})
-    List<Product> findByCategoryIdAndIdNotOrderByReviewCountDesc(Long categoryId, Long excludedProductId, Pageable pageable);
+    List<Product> findByCategoryIdAndIdNotOrderByReviewCountDesc(
+            Long categoryId,
+            Long excludedProductId,
+            Pageable pageable
+    );
+
+    // 조회순
+    @EntityGraph(attributePaths = {"category"})
+    List<Product> findByCategoryIdAndIdNotOrderByViewCountDesc(
+            Long categoryId,
+            Long excludedProductId,
+            Pageable pageable
+    );
+
+    // 조회수 증가 쿼리 메서드 추가
+    @Modifying
+    @Query("UPDATE Product p SET p.viewCount = p.viewCount + 1 WHERE p.id = :productId")
+    void increaseViewCount(@Param("productId") Long productId);
 
     @Query("""
                 SELECT p
