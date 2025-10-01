25b71f15ef682f97f68fb01c9df36f49457db3b2
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-10-02 08:51:54 +0900

    refactor: [review],[like] import문 수정

--- src/main/java/org/example/plusproject/domain/review/dto/request/ReviewSaveRequest.java
+++ src/main/java/org/example/plusproject/domain/review/dto/request/ReviewSaveRequest.java
@@ -3,9 +3,11 @@ package org.example.plusproject.domain.review.dto.request;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ReviewSaveRequest {
 
     @Min(1) @Max(5)
