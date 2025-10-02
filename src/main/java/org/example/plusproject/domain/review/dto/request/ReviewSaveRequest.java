c02e108c92cf3e122a3ecaa8f7c7ceea8c677d78
rlawprud <117605529+rlawprud@users.noreply.github.com>
2025-10-02 09:00:33 +0900

    refactor: [review] context Validation refactor
    refactor: [review] context Validation refactor

--- src/main/java/org/example/plusproject/domain/review/dto/request/ReviewSaveRequest.java
+++ src/main/java/org/example/plusproject/domain/review/dto/request/ReviewSaveRequest.java
@@ -3,6 +3,7 @@ package org.example.plusproject.domain.review.dto.request;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -13,6 +14,7 @@ public class ReviewSaveRequest {
     @Min(1) @Max(5)
     private int point;
 
+    @NotBlank
     @Size(min = 10, max = 1000)
     private String context;
 }
